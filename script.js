0dccd8e040489c7b06106cd6b17d3cb0f7a56fd1
Parth Goel <parthgoel1204@gmail.com>
2025-12-26 02:42:40 +0530

    feat:image preview added with back and forth navigation

--- script.js
+++ script.js
@@ -24,3 +24,38 @@ if (savedTheme === "dark") {
   themeIcon.classList.remove("fa-sun");
   themeIcon.classList.add("fa-moon");
 }
+
+const images = document.querySelectorAll(".pin img");
+// const gallery = document.querySelector(".gallery");
+
+const modal = document.querySelector("#imageModal");
+const imagePreview = document.querySelector("#modalImage");
+const closeBtn = document.querySelector(".close-btn");
+const nextBtn = document.querySelector(".nav-btn.next");
+const prevBtn = document.querySelector(".nav-btn.prev");
+
+let currentImageIndex = 0;
+
+images.forEach(function (img, index) {
+  img.addEventListener("click", function () {
+    modal.classList.remove("hidden");
+    imagePreview.src = this.src;
+    currentImageIndex = index;
+  });
+});
+
+closeBtn.addEventListener("click", function () {
+  modal.classList.add("hidden");
+});
+
+nextBtn.addEventListener("click", function () {
+  currentImageIndex = (currentImageIndex + 1) % images.length;
+  imagePreview.src = images[currentImageIndex].src;
+});
+
+prevBtn.addEventListener("click", function () {
+  currentImageIndex =
+    (currentImageIndex - 1 + images.length) % images.length;
+  imagePreview.src = images[currentImageIndex].src;
+});
+
